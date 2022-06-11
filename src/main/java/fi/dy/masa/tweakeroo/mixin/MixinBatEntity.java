4172422ab6cc38b268808f73721ddd76d79d8004
plusls <plusls@qq.com>
2022-06-11 13:38:53 +0800

    update to 1.19

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBatEntity.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinBatEntity.java
@@ -1,8 +1,6 @@
 package fi.dy.masa.tweakeroo.mixin;
 
-import java.util.Random;
-
-import net.minecraft.util.math.random.AbstractRandom;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +16,7 @@ import fi.dy.masa.tweakeroo.config.Configs;
 public abstract class MixinBatEntity
 {
     @Inject(method = "canSpawn", at = @At("HEAD"), cancellable = true)
-    private static void tweakeroo_disableBatSpawning(EntityType<BatEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, AbstractRandom random, CallbackInfoReturnable<Boolean> cir)
+    private static void tweakeroo_disableBatSpawning(EntityType<BatEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir)
     {
         if (Configs.Disable.DISABLE_BAT_SPAWNING.getBooleanValue())
         {
